b342545b73fc176ec2aef17d8345290705085490
York Lin <superj80820@gmail.com>
2018-10-07 17:15:47 +0800

    測試

--- web/js/main.js
+++ web/js/main.js
@@ -76,6 +76,7 @@ function gameloop() {
     if (o.bottom >= $("#land").offset().top) return void playerDead();
     var u = $("#ceiling");
     if (r <= u.offset().top + u.height() && (position = 0), null != pipes[0]) {
+        alert(pipe_item)
         pipe_item=Math.floor(Math.random()*pipe_item)
         var d = pipes[0],
             h = d.children(".pipe_upper"),
